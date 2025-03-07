deb30dc904f4192bc145351ed6083e996f4401a7
Tiziano Pacifico <gourmetmolise@gmail.com>
2025-03-07 22:57:22 +0100

    Fine component

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.scss']
+})
+export class FirstComponent {
+  name: string = "";
+  email: string = "";
+  message: string = "";
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.messages.push({
+      "name": this.name,
+      "email": this.email,
+      "message": this.message
+    });
+  }
+
+  deleteMessage(index: number) {
+    this.messages.splice(index, 1);
+  }
+
+  unsubmit() {
+    //this.isSubmitted = false;
+  }
+}
